0ba8ae7d7b1cb6aed17a9b43d06d0ad193469a4f
Restyled.io <commits@restyled.io>
2020-02-26 11:53:25 +0000

    Restyled by shellharden

--- gdal/ci/travis/csa_common/script.sh
+++ gdal/ci/travis/csa_common/script.sh
@@ -6,11 +6,11 @@ sudo apt-get install jq
 
 rm -f filtered_scanbuild.txt
 files=$(find gdal/scanbuildoutput -name "*.sarif")
-for f in $files; do
-    jq '.runs[].results[] | (if .locations[].physicalLocation.fileLocation.uri | (contains("/usr/include") or contains("degrib") or contains("libpng") or contains("libjpeg") or contains("EHapi") or contains("GDapi") or contains("SWapi") or contains("osr_cs_wkt_parser") or contains("ods_formula_parser") or contains("swq_parser") or contains("json_tokener") ) then empty else { "uri": .locations[].physicalLocation.fileLocation.uri, "msg": .message.text, "location": .codeFlows[-1].threadFlows[-1].locations[-1] } end)' < $f > tmp.txt
+for f in "$files"; do
+    jq '.runs[].results[] | (if .locations[].physicalLocation.fileLocation.uri | (contains("/usr/include") or contains("degrib") or contains("libpng") or contains("libjpeg") or contains("EHapi") or contains("GDapi") or contains("SWapi") or contains("osr_cs_wkt_parser") or contains("ods_formula_parser") or contains("swq_parser") or contains("json_tokener") ) then empty else { "uri": .locations[].physicalLocation.fileLocation.uri, "msg": .message.text, "location": .codeFlows[-1].threadFlows[-1].locations[-1] } end)' < "$f" > tmp.txt
     if [ -s tmp.txt ]; then
         echo "Errors from $f: "
-        cat $f
+        cat "$f"
         echo ""
         cat tmp.txt >> filtered_scanbuild.txt
     fi
